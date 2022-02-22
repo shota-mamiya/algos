ad2951a90a70ea127c11906a7ff3507c5b9b101f
Shota Mamiya <shota.mamiya@outlook.com>
2022-02-21 21:58:29 -0800

    contains duplicates with notes

--- containsDuplicates.js
+++ containsDuplicates.js
@@ -1,3 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+// ON^2 solution, slower runtime but better space complexity
+// 1852 ms, 48.5 mb
+
+ var containsDuplicate = function(nums) {
+
+
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i] == nums[j]) {
+          return true
+        }
+      } 
+    } return false
+};
+
+// O of N solution, faster but worse space complexity
+// 129 ms, 51.2 mb
+
 var containsDuplicate = function(nums) {
 
     let hashMap = {}
