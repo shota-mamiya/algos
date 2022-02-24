3d5199c24980189a417632a7f6374b3ab65e70b6
Shota Mamiya <shota.mamiya@outlook.com>
2022-02-23 18:28:38 -0800

    ValidParentheses

---
+++ ValidParentheses.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+let s = "({})"
+var isValid = function(s) {
+
+    // use an array as a "stack" to push the closing bracket to the end of the array
+    // when on an opening bracket, push the closing bracket to the end of the array
+
+    let stack = [] // creating new array to push the closing bracket and seeing if the next bracket matches the bracket at the end of array
+
+    if (s.length % 2 !== 0) return false;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") {
+            stack.push(")")
+        }
+        if (s[i] === "{") {
+            stack.push("}")
+        }
+        if (s[i] === "[") {
+            stack.push("]")
+        }
+        if (s[i] === ")" || s[i] === "}" || s[i] === "]") {
+            if (stack[stack.length - 1] === s[i]) {
+                stack.pop()
+            } else {
+                return false
+            }
+        }
+    }
+    if (stack.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+console.log(isValid(s));
